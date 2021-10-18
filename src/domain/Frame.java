245abbd5910f727accdac2ab82c38b62745242ce
seokj <seokj@DESKTOP-D3DODB7>
2021-10-18 22:15:29 +0900

    uploading project

--- src/domain/Frame.java
+++ src/domain/Frame.java
@@ -1,30 +1,6 @@
 package domain;
 
-public class Frame {
-	private static final int TOTAL_FRAME = 9;
-	private static final int NORMAL_FRAME = 2;
-	private static final int FINAL_FRAME = 3;
-	private static final int TOTAL_PINS = 10;
-
-	private int turn;
-	private int firstShot;
-
-	public Frame(int frameNumber) {
-		if(frameNumber == TOTAL_FRAME) {
-			turn = FINAL_FRAME;
-			return;
-		}
-		turn = NORMAL_FRAME;
-	}
-	public void calculateScore(int pinCnt) {
-		if(pinCnt == TOTAL_PINS) {
-			this.turn-=2;
-		}
-		this.turn--;
-	}
-	
-	public boolean hasTurn() {
-		return turn>0;
-	}
-
+public interface Frame {
+	boolean hasTurn();
+	void playBowling(int pinCnt);
 }
