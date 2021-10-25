399e7b10ff2a5aff8922aa2aba7a08393293c457
seokj <seokj@DESKTOP-D3DODB7>
2021-10-26 01:42:45 +0900

    getter and setter 구현

--- src/domain/Players.java
+++ src/domain/Players.java
@@ -4,6 +4,14 @@ public class Players {
 	private String name;
 	
 	public Players(String name) {
+		this.setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
 	}
 }
